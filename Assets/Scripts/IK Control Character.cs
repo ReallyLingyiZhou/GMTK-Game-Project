0c441c418b0b7e669b784e157dfac3398f49169e
Lingyi Zhou <lingyi@moviebill.com>
2024-08-17 13:52:20 -0400

    Update scene

--- Assets/Scripts/IK Control Character.cs
+++ Assets/Scripts/IK Control Character.cs
@@ -39,7 +39,7 @@ public class IKControlCharacter : MonoBehaviour
                 // Set the right hand target position and rotation, if one has been assigned
                 if(rightHandObj != null) {
                     animator.SetIKPositionWeight(AvatarIKGoal.RightHand,1);
-                    animator.SetIKRotationWeight(AvatarIKGoal.RightHand,1);  
+                    animator.SetIKRotationWeight(AvatarIKGoal.RightHand,0.5f);  
                     animator.SetIKPosition(AvatarIKGoal.RightHand,rightHandObj.position);
                     animator.SetIKRotation(AvatarIKGoal.RightHand,rightHandObj.rotation);
                 }
