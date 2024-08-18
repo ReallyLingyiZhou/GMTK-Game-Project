1f16b2025c35573540cee03734ad42d4a45336a9
GuyDigital <guy@digitaldotsinc.com>
2024-08-18 16:40:31 -0700

    Add InGameHud Checklist
    Add InGameHud Checklist

--- Assets/Scripts/ShortenStrikethrough.cs
+++ Assets/Scripts/ShortenStrikethrough.cs
@@ -7,38 +7,12 @@ public class ShortenStrikethrough : MonoBehaviour
 {
     public TextMeshProUGUI textMeshPro;
     public Image image;
-    public int maxText;
-    // private Vector3 scaleChange, positionChange;
-    // Start is called before the first frame update
     void Start()
     {
-        // gameObject.transform.localScale.x = gameObject.transform.localScale.x  * (float)textMeshPro.text.Length / (float)maxText;
-
-        // gameObject.transform.localScale += scaleChange;
-        // gameObject.transform.position += positionChange;
-
-        // // Move upwards when the sphere hits the floor or downwards
-        // // when the sphere scale extends 1.0f.
-        // if (gameObject.transform.localScale.y < 0.1f || gameObject.transform.localScale.y > 1.0f)
-        // {
-        //     scaleChange = -scaleChange;
-        //     positionChange = -positionChange;
-        // }
+        var curWidth = image.rectTransform.rect.size.x;
         var curScale = image.transform.localScale;
-        // var scaleMult = textMeshPro.preferredWidth;
-        var scaleMult = textMeshPro.text.Length / maxText;
-        image.transform.localScale = new Vector3(curScale.x * scaleMult, curScale.y, curScale.x);
+        var preferredWidth = textMeshPro.preferredWidth;
+        image.transform.localScale = new Vector3(curScale.x * preferredWidth / curWidth, curScale.y, curScale.x);
+        image.transform.localPosition = new Vector3(preferredWidth / 2, 0, 0);
     }
-    // private float GetTextActualBounds(){
-    //     // Text mesh size
-    //     foreach (char symbol in textMeshPro.text)
-    //     {
-    //     CharacterInfo info;
-    //     if (textMeshPro.font.(symbol, out info, textMeshPro.fontSize, textMeshPro.fontStyle))
-    //     {
-    //         width += info.advance;
-    //     }
-    //     }
-    //     return width * textMesh.characterSize * textMesh.transform.lossyScale.x * 0.1f;
-    // }
 }
