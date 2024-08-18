051582f6a6ca9238e2d0af34e9c3ff9698840047
GuyDigital <guy@digitaldotsinc.com>
2024-08-18 11:49:02 -0700

    PauseMenuPrefab Added
    PauseMenuPrefab Added

---
+++ Assets/UI/ClockOutMenu/FinishPoint.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FinishPoint : MonoBehaviour
+{
+    // private void OnControllerColliderHit(ControllerColliderHit hit)
+    // {
+    //     SceneManager.LoadSceneAsync(0);
+    //     UnlockNewLevel();
+    // }
+    private void OnCollisionEnter(Collision collision)
+    {
+        // if (collision.gameObject.tag == "Player")
+        // {
+        SceneManager.LoadSceneAsync(0);
+        UnlockNewLevel();
+        // }
+    }
+    void UnlockNewLevel()
+    {
+        if (SceneManager.GetActiveScene().buildIndex >= PlayerPrefs.GetInt("ReachedIndex"))
+        {
+            PlayerPrefs.SetInt("ReachedIndex", SceneManager.GetActiveScene().buildIndex + 1);
+            PlayerPrefs.SetInt("UnlockedLevel", PlayerPrefs.GetInt("UnlockedLevel", 1) + 1);
+            PlayerPrefs.Save();
+        }
+    }
+}
